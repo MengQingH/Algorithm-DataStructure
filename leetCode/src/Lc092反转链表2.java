ed83d220a1d00e7b3c1a8116a05cd2b93496bad7
qinghui_meng <qinghui_meng@kingdee.com>
2021-02-23 20:26:57 +0800

    递归‘

--- leetCode/src/Lc092反转链表2.java
+++ leetCode/src/Lc092反转链表2.java
@@ -1,7 +1,40 @@
-public class Lc092 {
+/**
+ *
+ */
+public class Lc092反转链表2 {
 
-    public ListNode reverseBetween(ListNode head, int left, int right){
-        
+    /**
+     * 反转索引从m到n的节点，如果m = 1，那么就相当于反转前n个节点，可以从第m个元素开始进行反转，此时就相当于反转前n-m个元素。
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left == 1){
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left-1, right-1);
+        return head;
+    }
+
+    private ListNode successor = null;
+
+    /**
+     * 反转链表的前n个节点
+     * @param head
+     * @param n
+     * @return
+     */
+    private ListNode reverseN(ListNode head, int n){
+        if(n == 1){
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n-1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
     }
 
 
