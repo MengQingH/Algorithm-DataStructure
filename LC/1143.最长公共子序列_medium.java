e87d7b76e3bbd49cdb4a4fc3a029011948fb2cba
MengQingH <mengqh98@qq.com>
2021-04-27 10:28:31 +0800

    dp

---
+++ LC/1143.最长公共子序列_medium.java
@@ -0,0 +1,28 @@
+/*
+ * @Author: QingHui Meng
+ * @Date: 2021-04-26 16:01:43
+ */
+/*
+ * @lc app=leetcode.cn id=1143 lang=java
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * 归纳：已知dp[0..i-1][]
+     * 
+     * dp数组的定义：dp[i][j]表示text1[0...i]和text2[0...j]中的最长公共子序列为dp[i][j]
+     * 
+     * @param {String} text1
+     * @param {String} text2
+     * @return {*}
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+
+    }
+}
+// @lc code=end
+
--- LC/DP/322.零钱兑换_medium.java
+++ LC/DP/322.零钱兑换_medium.java
@@ -11,10 +11,53 @@
 // @lc code=start
 class Solution {
 
-    Map<Integer, Integer> 
-
+    /**
+     * dp数组定义：dp[i]表示凑出i需要的最少的零钱。
+     * 归纳：dp[i]的最小值就是min(dp[i-coin]+1)
+     * 
+     * @param {int[]} coins
+     * @param {int} amount
+     * @return {*}
+     */
     public int coinChange(int[] coins, int amount) {
-        
+        int[] dp = new int[amount+1];
+        // dp[i]的最大值为amount，把所有的值设置为amount+1方便求最小值
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for(int i = 0; i<=amount; i++){
+            for(int coin : coins){
+                if(i-coin >= 0)
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+            }
+        }
+        return dp[amount] > dp ? -1 : dp[amount];
+    }
+
+    /**
+     * 效率较低
+     * 
+     * @param {int[]} coins
+     * @param {int} amount
+     * @return {*}
+     */
+    public int coinChange1(int[] coins, int amount) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(0, 0);
+        for(int i = 1; i<=amount; i++){
+            int min = -1;
+            for(int coin : coins){
+                int count = memo.getOrDefault(i-coin, -1);
+                if(count == -1)
+                    continue;
+                if(min == -1)
+                    min = count+1;
+                else {
+                    min = Math.min(min, count+1);
+                }
+            }
+            memo.put(i, min);
+        }
+        return memo.get(amount);
     }
 }
 // @lc code=end
